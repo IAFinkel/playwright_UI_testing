33976656152e08522fa27b25e1b6876a8652efc3
IAFinkel <gorbunov.nk@mail.ru>
2025-09-30 15:48:14 +0300

    fix config

--- playwright.config.ts
+++ playwright.config.ts
@@ -64,6 +64,8 @@ export default defineConfig<TestOptions>({
 
   webServer:{
     command: 'npm run start',
-    url: 'http://localhost:4200/' 
+    url: 'http://localhost:4200/',
+    timeout: 120000,
+    reuseExistingServer: !process.env.CI,
   }
 });
